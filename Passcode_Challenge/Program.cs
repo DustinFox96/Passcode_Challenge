271ba03a97a31f16e73a4b72c6ab75718015c7a0
Dustin Fox <djf149@hotmail.com>
2021-06-16 14:56:08 -0400

    Add project files.

---
+++ Passcode_Challenge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Passcode_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the pass code?");
+            var code = Console.ReadLine().ToLower();
+
+            if(code == "secret")
+            {
+                Console.WriteLine("Access granted");
+            }
+            else if(code != "secret")
+            {
+                Console.WriteLine("Access Denied, Please insert correct Pass Code");
+            }else if(code == "fake pass code")
+            {
+                Console.WriteLine("Nice try");
+            }
+
+        }
+    }
+}
